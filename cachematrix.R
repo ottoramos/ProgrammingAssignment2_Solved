441741d0993360e065623bdc79e0c006dcd6b67f
ottoramos <orcastro@reduno.com.mx>
2016-01-13 14:43:08 -0600

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,4 +1,4 @@
-##     TESTING  COMMIT
+##     TESTING  COMMIT 2
 ## Put comments here that give an overall description of what your
 ## functions do
 
@@ -12,5 +12,5 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-  ## Return a matrix that is the inverse of 'x'
+        ## Return a matrix that is the inverse of 'x'
 }
